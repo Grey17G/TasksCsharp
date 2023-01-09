71ea7489ceb992685768e32428e533fff7c6057a
Irina <irina.gregorjevskaya@gmail.com>
2023-01-09 18:38:05 +0300

    Add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine ("введите трехзначное число");
+int number = int.Parse (Console.ReadLine ()!);
+
+int FindSecondDigit(int number)
+{
+    int result = (number/10)%10;
+    return result;
+}
+
+int dig2 = FindSecondDigit(number);
+Console.WriteLine ($"Вторая цифра числа {number} равно {dig2}");
+
+
