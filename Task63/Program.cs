7366d3440c43a4653c8070ed744c282c9b6bf161
Irina <irina.gregorjevskaya@gmail.com>
2023-02-18 23:15:23 +0300

    Add Task 63

---
+++ Task63/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+(Решено с помощью рекурсии)*/
+
+int getFromUserData (string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine (message);
+    Console.ResetColor();
+    int result = int. Parse (Console.ReadLine()!);
+    return result;
+}
+
+int UserNumber = getFromUserData ("Введите значение для границы ряда");
+
+printRange (UserNumber);
+
+void printRange (int N)
+{
+    if (N ==0)
+    {
+        return;
+    }
+
+    printRange(N-1);
+    Console.Write(N + ",");
+}
